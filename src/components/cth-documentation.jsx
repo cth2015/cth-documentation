1bc7fbb4fed7c655b01635b9eb832c972473babd
Zaturrby <rjkorteschiel@gmail.com>
2015-11-16 17:41:11 +0100

    #5 first templating

--- src/components/cth-documentation.jsx
+++ src/components/cth-documentation.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import Navigation from "./navigation/navigation-top.jsx"
+import Footer from "./footer/footer.jsx"
+import Post from "./post/post.jsx"
+
+
 class CthDocumentation extends React.Component{
 	constructor(props){
 		super(props);
@@ -7,7 +12,13 @@ class CthDocumentation extends React.Component{
 
     render() {
  		return (
- 			<div> CTH - Documentation </div>
+ 			<div className="app-container">
+ 				<Navigation />
+ 				{[0,1,2,3,4].map(function(content, key){
+ 					return (<Post key={key} content={content}/>)
+ 				})}
+ 				<Footer />
+			</div>
  		)
  	}
 }
---
+++ src/components/navigation/navigation-top.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Navigation extends React.Component{
+    render() {
+ 		return (
+ 			<div> Nav-Top </div>
+ 		)
+ 	}
+}
+
+export default Navigation;
