c858c38398178d53fed8a908605154b8012cc7d9
Zaturrby <rjkorteschiel@gmail.com>
2015-12-24 00:37:13 +0100

    #10 #11 small changes in stle and comment removal

--- src/components/pages/page.jsx
+++ src/components/pages/page.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 class Page extends React.Component{
     render() {
-    	let regexp = /\w+[/]/;
-    	console.dir(regexp.exec('courses/1'));
-    	// console.log(this.props.routeParams.id);
-    	// console.log(this.props.appData.)
+  	let regexp = /\w+[/]/;
+  	console.dir(regexp.exec('courses/1'));
  		return (
  			<main className="page"> 
  				<h1>PAGES</h1>
